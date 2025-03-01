38aacf262541fc190a1c01afd534fe617c6277db
FTM DEVELOPER <106005123+ftmdeveloperz@users.noreply.github.com>
2025-03-01 19:34:30 +0530

    Update genthumb.py

--- Youtube/genthumb.py
+++ Youtube/genthumb.py
@@ -1,6 +1,3 @@
-# ©️ LISA-KOREA | @LISA_FAN_LK | NT_BOT_CHANNEL | LISA-KOREA/YouTube-Video-Download-Bot
-
-# [⚠️ Do not change this repo link ⚠️] :- https://github.com/LISA-KOREA/YouTube-Video-Download-Bot
 
 from pyrogram import Client, filters
 import yt_dlp
